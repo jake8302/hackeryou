4b2fc7e6cd7f01971701df3911179e12b1bfce86
jake8302 <39091828+jake8302@users.noreply.github.com>
2019-01-17 19:44:11 -0500

    changed css to follow style guide

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,47 +1,52 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
 import logo from './tiff.svg';
 import './App.css';
 
-var discover_url = "https://api.themoviedb.org/3/discover/movie?api_key=041ff7fe3df8f5abf78dd2b4cd34912a&language=en-US&sort_by=release_date.desc&include_adult=false&include_video=false&page=1&primary_release_year=2019&vote_count.gte=10" 
 var imgSrc = "https://image.tmdb.org/t/p/w400"
 class App extends Component {
-  data = [];
   constructor(props) {
     super(props);
+    this.movieInfo = {
+      title: [],
+      poster_path: []
+    }
     this.state = {
       error: null,
       isLoaded: false,
-      items: []
     };
+
+    this.test = {
+      rows:[]
+    }
   }
 
   componentDidMount() {
-    // For initial data
     this.fetchData();
   }
 
   fetchData = () => {
-    fetch(discover_url)
+    const discoverUrl = "https://api.themoviedb.org/3/discover/movie?api_key=041ff7fe3df8f5abf78dd2b4cd34912a&language=en-US&sort_by=release_date.desc&include_adult=false&include_video=false&page=1&primary_release_year=2019&vote_count.gte=10" 
+    fetch(discoverUrl)
       .then(res => res.json())
       .then(
         (result) => {
-          console.log(result);
-          console.log(result.results[0].title);
-          console.log(Object.keys(result.results));
-          console.log(Object.keys(result.results).length);
-          for (let i = 0; i < Object.keys(result.results).length; i++) {
-            this.data.push(imgSrc + result.results[i].poster_path);
-          }
+          console.log(result.results);
+          const movie = result.results;
+          var movieRows = [];
+          movie.forEach(movie => {
+            movie.poster_src = "https://image.tmdb.org/t/p/w400" + movie.poster_path;
+
+            const movieRow = <img src={movie.poster_src}></img>;
+           
+            movieRows.push(movieRow);
+          });
+          this.test = {rows: movieRows};
+          console.log(this.movieInfo);
 
           this.setState({
             isLoaded: true,
-            items: result.items
           });
         },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
         (error) => {
           this.setState({
             isLoaded: true,
@@ -49,49 +54,28 @@ class App extends Component {
           });
         }
       )
-      
+
   }
   
-  buttonClick() {
-    alert("pewpewpepwpewppwe");
-  }
-
-  getImgUrl=(imageNum)=> {
-    return <img src={this.data[imageNum]}></img>;
-  }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://developers.themoviedb.org/3/movies/get-movie-details"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            API KEY
-          </a>
-          <button onClick={this.fetchData}>Useless Button</button>
-        </header>
-        <div id="App-movies">
-          {this.getImgUrl(0)}
-          {this.getImgUrl(1)}
-          {this.getImgUrl(2)}
-          {this.getImgUrl(3)}
-          {this.getImgUrl(4)}
-          {this.getImgUrl(5)}
-          {this.getImgUrl(6)}
-          {this.getImgUrl(7)}
-          {this.getImgUrl(8)}
-          {this.getImgUrl(9)}
-          {this.getImgUrl(10)}
-          {this.getImgUrl(11)}
-          {this.getImgUrl(12)}
-          {this.getImgUrl(13)}
+        <table className="titleBar">
+          <tbody>
+            <tr>
+              <td>
+                <img height="50vmin" src={logo}></img>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="title">
+          <h1>Latest Release From 2019!</h1>
         </div>
-
+        <div className="moviesBody">
+          {this.test.rows}
+        </div>
       </div>
     );
   }
