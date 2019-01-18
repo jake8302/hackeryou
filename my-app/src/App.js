6fe813b5ca841e9f57dfb67fc6e1e77ba5771894
jake8302 <39091828+jake8302@users.noreply.github.com>
2019-01-17 20:17:25 -0500

    now fetch all nesscary data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,9 +15,7 @@ class App extends Component {
       isLoaded: false,
     };
 
-    this.test = {
-      rows:[]
-    }
+    this.moviePoster = []
   }
 
   componentDidMount() {
@@ -25,7 +23,7 @@ class App extends Component {
   }
 
   fetchData = () => {
-    const discoverUrl = "https://api.themoviedb.org/3/discover/movie?api_key=041ff7fe3df8f5abf78dd2b4cd34912a&language=en-US&sort_by=release_date.desc&include_adult=false&include_video=false&page=1&primary_release_year=2019&vote_count.gte=10" 
+    const discoverUrl = "https://api.themoviedb.org/3/discover/movie?api_key=041ff7fe3df8f5abf78dd2b4cd34912a&language=en-US&sort_by=release_date.desc&include_adult=false&include_video=false&page=1&primary_release_year=2019&vote_count.gte=10";
     fetch(discoverUrl)
       .then(res => res.json())
       .then(
@@ -35,14 +33,19 @@ class App extends Component {
           var movieRows = [];
           movie.forEach(movie => {
             movie.poster_src = "https://image.tmdb.org/t/p/w400" + movie.poster_path;
-
-            const movieRow = <img src={movie.poster_src}></img>;
-           
+            const movieRow = <img src={movie.poster_src} hspace="20" vspace="20"></img>;
             movieRows.push(movieRow);
+            const testUrl = `https://api.themoviedb.org/3/movie/${movie.id}?api_key=041ff7fe3df8f5abf78dd2b4cd34912a&language=en-US`;
+            fetch(testUrl)
+            .then(res => res.json()
+            .then(
+              (result) => {
+                console.log(result.overview);
+              }
+            ));
           });
-          this.test = {rows: movieRows};
+          this.moviePoster = movieRows;
           console.log(this.movieInfo);
-
           this.setState({
             isLoaded: true,
           });
@@ -74,7 +77,7 @@ class App extends Component {
           <h1>Latest Release From 2019!</h1>
         </div>
         <div className="moviesBody">
-          {this.test.rows}
+          {this.moviePoster}
         </div>
       </div>
     );
